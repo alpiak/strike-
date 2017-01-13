9291b4b07fcfb9af473fa0714e09c2def51bbb43
erya <hang3126@sina.com>
2017-01-13 18:26:25 +0800

    add HtmlWebpackPlugin

---
+++ src/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by qhyang on 2017/1/13.
+ */
+const Matter = require("matter-js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
-var webpack = require('webpack');
+const webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: 'src/index.js',
+    entry: './src/index.js',
     output: {
-        path: 'build',
+        path: './build',
         filename: 'index_bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: 'src/index.html'
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+                template: './src/index.html'
+        })
+    ]
 };
